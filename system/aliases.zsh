c2c3a0713bc5556f52497d7977542bac6068fd64
Colin Daly <mr.colin.daly@gmail.com>
2015-07-26 23:38:03 +1000

    Remove more stuff I don't want.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,19 +1,8 @@
 #!/bin/sh
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-# shellcheck disable=SC2039
-if gls &>/dev/null; then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la="gls -A --color"
-else
-  alias ls="ls -F --color"
-  alias l="ls -lAh --color"
-  alias ll="ls -l --color"
-  alias la="ls -A --color"
-fi
+alias ls="ls -F --color"
+alias l="ls -lAh --color"
+alias ll="ls -l --color"
+alias la="ls -A --color"
 alias grep="grep --color=auto"
 alias duf="du -sh * | sort -hr"
 alias less="less -r"
@@ -28,10 +17,8 @@ if [ -z "$(command -v pbcopy)" ]; then
   fi
 fi
 
-if [ "$(uname -s)" != "Darwin" ]; then
-  if [ -e /usr/bin/xdg-open ]; then
-    alias open="xdg-open"
-  fi
+if [ -e /usr/bin/xdg-open ]; then
+  alias open="xdg-open"
 fi
 
 # greps non ascii chars
